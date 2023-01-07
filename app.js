1600f7290f9f84ceb6ce77c529de50640ccad95b
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-01-07 18:56:08 +0100

    Move post routes to the routes/postRoutes.js

--- app.js
+++ app.js
@@ -30,61 +30,6 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/posts/create', (req, res) => {
-    res.render('create');
-});
-
-// Post routing
-app.get('/posts', (req, res) => {
-    // Display all posts
-    Post.find()
-        .then(result => {
-            res.render('index', { posts: result });
-        })
-        .catch(err => {
-            console.error(err);
-        });
-});
-
-app.post('/posts', (req, res) => {
-    // Save form data to the database
-    const post = new Post(req.body);
-
-    post.save()
-        .then(result => {
-            res.redirect('/posts');
-        })
-        .catch(err => {
-            console.error(err);
-        });
-});
-
-// Get each post by id parameter
-app.get('/posts/:id', (req, res) => {
-    const id = req.params.id;
-
-    Post.findById(id)
-        .then(result => {
-            res.render('details', { post: result });
-        })
-        .catch(err => {
-            console.error(err);
-        });
-});
-
-// Delete post by id parameter
-app.delete('/posts/:id', (req, res) => {
-    const id = req.params.id;
-
-    Post.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/posts' });
-        })
-        .catch(err => {
-            console.error(err);
-        });
-});
-
 app.use((req, res) => { // redirects any invalid request to a 404 page
     res.status(404).render('404');
 });
