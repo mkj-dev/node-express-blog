cee2a2b166894ca50e19368d574938879b7bdccf
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-01-07 18:56:27 +0100

    Add post routes

---
+++ routes/postRoutes.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+
+// Post routing
+router.get('/posts/create', (req, res) => {
+    res.render('create');
+});
+
+router.get('/posts', (req, res) => {
+    // Display all posts
+    Post.find()
+        .then(result => {
+            res.render('index', { posts: result });
+        })
+        .catch(err => {
+            console.error(err);
+        });
+});
+
+router.post('/posts', (req, res) => {
+    // Save form data to the database
+    const post = new Post(req.body);
+
+    post.save()
+        .then(result => {
+            res.redirect('/posts');
+        })
+        .catch(err => {
+            console.error(err);
+        });
+});
+
+// Get each post by id parameter
+router.get('/posts/:id', (req, res) => {
+    const id = req.params.id;
+
+    Post.findById(id)
+        .then(result => {
+            res.render('details', { post: result });
+        })
+        .catch(err => {
+            console.error(err);
+        });
+});
+
+// Delete post by id parameter
+router.delete('/posts/:id', (req, res) => {
+    const id = req.params.id;
+
+    Post.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/posts' });
+        })
+        .catch(err => {
+            console.error(err);
+        });
+});
+
+module.exports = router;
